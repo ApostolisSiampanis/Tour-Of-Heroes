ee8b7f8f3471b4490c9e215a5f06c03e1edeebb9
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-12-14 22:45:21 +0200

    Update app.module.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import {Hero} from '../hero';
-import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 import {
   NgFor,
@@ -17,24 +16,17 @@ import {
   imports: [
     FormsModule,
     NgFor,
-    HeroDetailComponent
+    RouterModule
+
   ]
 })
 export class HeroesComponent {
-
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`); 
   }
 
   getHeroes(): void {
