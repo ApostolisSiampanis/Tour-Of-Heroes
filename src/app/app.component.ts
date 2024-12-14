c6b758d8666d6c02a11737a121ee7f9908f8a61e
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-12-14 14:57:54 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
